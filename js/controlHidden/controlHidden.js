e923c748a9b43d95765855e407af007c4f3d81fd
connorwilsonexceleron <connor.wilson@excelerondesigns.com>
2020-01-09 14:58:58 -0500

    ♻️💡
    adds documentation for config object
    begins start of styles generator

--- js/controlHidden/controlHidden.js
+++ js/controlHidden/controlHidden.js
@@ -4,11 +4,12 @@ const controls = [
 	...document.getElementsByClassName(class_prefix + 'conditional')
 ];
 
+/*
 function createConfig(controller) {
 	const {
 		dataset: {
 			hideControls: hideId,
-			hideEvent = 'change' /* jshint ignore:line */,
+			hideEvent = 'change',
 			hideExpects
 		}
 	} = controller;
@@ -43,33 +44,15 @@ function changeVisibilityForTargets({
 	controller: { value },
 	expects
 }) {
-	// jshint ignore:line
-
-	/*
-	 * if there is no expected value
-	 * or
-	 * if the current value is the same as the expected value
-	 *
-	 * then change the visibility of all the targets
-	 */
-
 	if (expects === undefined || value === expects) {
 		return targets.forEach(target => changeVisibility(target));
 	}
-
-	/*
-	 * else if the value is not the same as expected value
-	 *
-	 * hide all targets
-	 */
 	if (value !== expects)
 		return targets.forEach(target => changeVisibility(target, true));
 
-	/* check if we're still developing the website */
-
 	return;
 }
-/*
+
 function changeVisibility(target, shouldBeHidden) {
 	console.log(shouldBeHidden);
 	if (shouldBeHidden !== undefined)
@@ -87,12 +70,50 @@ class controlHidden {
 	 */
 	constructor(options) {
 		this.DEFAULTS = {
-			class_prefix: 'js-hide_',
+			debug: true,
+			css: true,
+			prefix: 'js-hide_',
 			input_suffix: 'input',
 			conditional_suffix: 'conditional',
-			debug: true
+			get inputs() {
+				return [...this.byClass(this.prefix + this.input_suffix)];
+			},
+			get conditionals() {
+				return [...this.byClass(this.prefix + this.conditional_suffix)];
+			}
 		};
-		CONFIG = Object.assign(this.DEFAULTS, options);
+		/**
+		 * CONFIG
+		 * @param debug turn on or off errors
+		 * @param css if true, css styles will default to plugin else ignored
+		 * @param prefix use a custom class prefix
+		 * * This plugin takes advantage of separate suffixes for separate uses
+		 * * input_suffix is aimed at inputs that control parts of the page but will not be seen
+		 * * conditional_suffix can be interacted with and therefore can be seen
+		 * @param input_suffix use a custom class suffix
+		 * @param conditional_suffix use a custom class suffix
+		 *
+		 * @param inputs takes an array of DOM elements to watch
+		 * @param conditionals takes an array of DOM elements to watch and evaluate
+		 */
+		this.CONFIG = Object.assign(this.DEFAULTS, options);
+
+		//* Generates an array of DOM Elements from the given arrays
+		this.controls = [...this.CONFIG.inputs, ...this.CONFIG.conditionals];
+
+		//* Checks to see if styles need to be added
+		this.CONFIG.css && this.addCSS();
+	}
+
+	addCSS() {
+		const el = document.createElement('style');
+		el.innerText = `
+        
+        `;
+	}
+
+	byClass(str) {
+		return document.getElementsByClassName(str);
 	}
 
 	changeVisibility(target, shouldBeHidden) {
@@ -102,11 +123,39 @@ class controlHidden {
 
 		/* Otherwise, toggle hidden attribute */
 		return !target.hidden
-			? (target.hidden = true)
-			: (target.hidden = false);
+			? target.setAttribute('hidden', true)
+			: target.setAttribute('hidden', false);
 	}
 
-	applyControllers() {}
+	generateConfig(controller) {
+		const {
+			dataset: { hideControls: hideId, hideEvent = 'change', hideExpects }
+		} = controller;
+
+		return {
+			controller,
+			name: controller.getAttribute('name'),
+			controls: hideId,
+			targets: [
+				...document.getElementsByClassName(
+					this.CONFIG.class_prefix + hideId
+				)
+			],
+			event: hideEvent,
+			expects: hideExpects
+		};
+	}
+
+	applyControllers(controllers) {
+		const configs = controllers.map(this.generateConfigs);
+
+		configs.forEach(config =>
+			config.controller.addEventListener(config.event, () =>
+				this.changeVisibilityForTargets(config)
+			)
+		);
+	}
+
 	changeVisibilityForTargets(controllerConfig) {
 		const {
 			targets,
@@ -137,6 +186,9 @@ class controlHidden {
 
 		/*? check if we're still developing the website */
 
+		/*
+		 * TODO: Add debug messaging for dev mode
+		 */
 		return;
 	}
 }
