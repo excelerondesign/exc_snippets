021e9b841ba67f3364f5885ce2d17852084603e3
connorwilsonexceleron <connor.wilson@excelerondesigns.com>
2020-01-09 15:21:59 -0500

    finishes up v1.0.0 of the class
    things left to  do
    - figure out some babel plan
    - document in a readme
    - minification

--- js/controlHidden/controlHidden.js
+++ js/controlHidden/controlHidden.js
@@ -1,68 +1,4 @@
-const class_prefix = 'js-hide_';
-const controls = [
-	...document.getElementsByClassName(class_prefix + 'input'),
-	...document.getElementsByClassName(class_prefix + 'conditional')
-];
-
-/*
-function createConfig(controller) {
-	const {
-		dataset: {
-			hideControls: hideId,
-			hideEvent = 'change',
-			hideExpects
-		}
-	} = controller;
-
-	return {
-		controller,
-		name: controller.getAttribute('name'),
-		controls: hideId,
-		targets: [...document.getElementsByClassName(class_prefix + hideId)],
-		event: hideEvent,
-		expects: hideExpects
-	};
-}
-
-function applyControllers(arr) {
-	const configs = arr.map(createConfig);
-
-	console.group('Check configs');
-	configs.forEach(config => console.log(config.name, config));
-	console.groupEnd();
-	console.log('End configs');
-
-	configs.forEach(config =>
-		config.controller.addEventListener(config.event, () =>
-			changeVisibilityForTargets(config)
-		)
-	);
-}
-
-function changeVisibilityForTargets({
-	targets,
-	controller: { value },
-	expects
-}) {
-	if (expects === undefined || value === expects) {
-		return targets.forEach(target => changeVisibility(target));
-	}
-	if (value !== expects)
-		return targets.forEach(target => changeVisibility(target, true));
-
-	return;
-}
-
-function changeVisibility(target, shouldBeHidden) {
-	console.log(shouldBeHidden);
-	if (shouldBeHidden !== undefined)
-		return target.setAttribute('hidden', shouldBeHidden);
-
-	return !target.hidden ? (target.hidden = true) : (target.hidden = false);
-}
-*/
-applyControllers(controls);
-
+//TODO: figure out a better name...
 class controlHidden {
 	/**
 	 * optional config object to pass in
@@ -147,11 +83,7 @@ class controlHidden {
 			controller,
 			name: controller.getAttribute('name'),
 			controls: hideId,
-			targets: [
-				...document.getElementsByClassName(
-					this.CONFIG.class_prefix + hideId
-				)
-			],
+			targets: [...this.byClass(this.CONFIG.class_prefix + hideId)],
 			event: hideEvent,
 			expects: hideExpects
 		};
@@ -195,11 +127,7 @@ class controlHidden {
 				this.changeVisibility(target, true)
 			);
 
-		/*? check if we're still developing the website */
-
-		/*
-		 * TODO: Add debug messaging for dev mode
-		 */
+		//TODO: todo Add debug messaging for dev mode
 		return;
 	}
 }
