5549237a9eb2ef32567be0fde25e365c3b0272ed
Connor Wilson <26461046+frankiekie@users.noreply.github.com>
2020-06-01 10:20:38 -0400

    💄 updates script to include accessiblity changes
    Changes from string manipulation to DomDocument
    adds aria-label and aria-hidden based on label inclusion
    
    Automatically adds a image role attribute (maybe this needs to be changed
    for accessibility reasons?)
    
    Also includes extra random attributes if fields are
    ignored

--- php/getsvg/getsvg.php
+++ php/getsvg/getsvg.php
@@ -1,53 +1,38 @@
 <?php
-if (!$input) return 'No Input';
-
-// create a random 5 char string
-$rand = hash('ripemd160', rand(1, 100));
-$rand = substr($rand, 5, 5);
-
-// get script properties
-$id = $modx->getOption('id', $scriptProperties, "svg_{$rand}");
-$classes = $modx->getOption('classes', $scriptProperties, '');
-$debug = $modx->getOption('debug', $scriptProperties, false);
-$alt = $modx->getOption('alt', $scriptProperties, '');
-// get file contents
-$svg = file_get_contents($input);
-
-// checks the mime content type of the input to make sure it is an SVG
-$isSvgMime = mime_content_type($input) === 'image/svg+xml';
-
-// checks if the xmlns link is present
-$isSvgContent = preg_match('/xmlns="http:\/\/www.w3.org\/2000\/svg"/', $svg, $output_array);
-
-// checks if is an svg element
-$isSvgEl = preg_match('/<svg.*\/svg>/', $svg, $output_array);
-
-$hasID = preg_match('/ id=/', $svg, $output_array);
-// $modx->log(modX::LOG_LEVEL_ERROR, );
-// Tests logic above
-if ($isSvgMime || $isSvgContent || $isSvgEl) {
-
-    // Removes HTML comments
-    $svg = preg_replace("/<!--(.|\\s)*?-->/", "", $svg);
-
-    // Removes the xml tag
-    $svg = preg_replace("/<\?(.|\\s)*?\?>/", "", $svg);
-
-    if ($hasID) {
-        // if it has an id replace it with a random one
-        $svg = preg_replace("/id=\"(?:(?!\").)*?\"/", "id='{$id}'", $svg, 1);
-    } else {
-        $svg = preg_replace("/>/", "id='{$id}'>", $svg, 1);
+if (!function_exists('assign_attr')) {
+    function assign_attr($el, $attr, $val)
+    {
+        $el->setAttribute($attr, $val);
     }
-    if ($hasAlt) {
-        $svg = preg_replace("/>", "aria-label='{addslashes($alt)}'");
-    }
-    $svg = preg_replace("/>/", " class='svg svg--replaced {$classes}'>", $svg, 1);
-
-    // return it to the page
-    $output = $svg;
-} else {
-    // otherwise just return the string below
-    $output = "Not an SVG: {$input}";
 }
-return $output;
+
+$input = $modx->getOption('input', $scriptProperties, '');
+$title = $modx->getOption('title', $scriptProperties, '');
+$label = $modx->getOption('label', $scriptProperties, '');
+
+$rand = rand(1, 100);
+
+$id_default = 'svg_' . $rand;
+$id = $modx->getOption('id', $scriptProperties, $id_default);
+
+$class_default = 'svg-' . $rand;
+$classes = $modx->getOption('classes', $scriptProperties, $class_default);
+$classNames = 'svg svg--replaced ' . $classes;
+
+$doc = new DomDocument;
+
+if (!$doc->Load($input)) return "Not a valid XML document";
+
+$tag = $doc->getElementsByTagName('svg')->item(0);
+
+if (!empty($label)) {
+    assign_attr($tag, 'aria-label', $label);
+} else {
+    assign_attr($tag, 'aria-hidden', 'true');
+}
+
+assign_attr($tag, 'id', $id);
+assign_attr($tag, 'role', 'image');
+assign_attr($tag, 'class', $classNames);
+
+return $doc->saveXML();
