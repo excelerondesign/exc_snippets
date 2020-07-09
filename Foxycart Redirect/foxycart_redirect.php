c9a0d717fba7b41b41f5b3806a5d7cfc6ba5831c
Ryan Babka <50176876+4rch1m3d35@users.noreply.github.com>
2020-07-09 15:49:54 -0400

    Formatted incoming strings

--- Foxycart Redirect/foxycart_redirect.php
+++ Foxycart Redirect/foxycart_redirect.php
@@ -1,11 +1,13 @@
+<?php
+
 /*!
  * MODX Foxycart Redirect FormIt Hook
  * (c) 2020 Exceleron Designs, MIT License, https://excelerondesigns.com
  * Author: Ryan Babka
  */
 
-<?php
 $value_array = $_POST;
+
 
 if(empty($foxycart_subdomain)){
     $modx->log(modX::LOG_LEVEL_ERROR, "Must include subdomain \'subdomain.foxycart.com\'.");
@@ -20,10 +22,14 @@ if($empty)  $parameter_array[] = "empty=true";
 
 if(!empty($cart)) $parameter_array[] = "cart={$cart}";
 
+$remove_values = preg_replace("/\s+/", "", $remove_values);
+
 $remove = explode(',', $remove_values);
 foreach($remove as $value) {
     unset($value_array[$value]);
 }
+
+$hidden_values = preg_replace("/\s+/", "", $hidden_values);
 
 $hidden = explode(',', $hidden_values);
 
@@ -34,10 +40,9 @@ foreach($value_array as $key => $value){
     $parameter_array[] = "{$key}={$value}";
 }
 
-
 if($foxycart_debug) {
     $modx->log(modX::LOG_LEVEL_ERROR, 'Base URL: ' . $url);
-    $modx->log(modX::LOG_LEVEL_ERROR, 'Parameter Values:\n' . print_r($parameter_array, true));
+    $modx->log(modX::LOG_LEVEL_ERROR, 'Parameter Values: ' . print_r($parameter_array, true));
 }
 
 $parameter_string = implode('&', $parameter_array);
